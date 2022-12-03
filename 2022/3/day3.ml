118933325ed17f2013f7575fe51cb3b35f46a8c5
octachron <octa@polychoron.fr>
2022-12-03 11:19:38 +0100

    Day 3

---
+++ 2022/3/day3.ml
@@ -0,0 +1,60 @@
+module Input = Helper.Input
+
+let failf fmt = Format.kfprintf (fun ppf -> Format.fprintf ppf "@."; exit 2) Format.err_formatter fmt
+module Char_set = Set.Make(Char)
+
+
+let score = function
+  | 'a'..'z' as c ->  Char.code c - Char.code 'a' + 1
+  | 'A' .. 'Z' as c -> Char.code c - Char.code 'A' + 27
+  | c -> failf "Invalid character:%c" c
+
+
+let to_set s = String.fold_left (fun set c -> Char_set.add c set) Char_set.empty s
+
+module Part_1() = struct
+  let parse s =
+    let n = String.length s in
+    let half =
+      if n mod 2 = 1 then failf "Odd string length for %S" s
+      else n/2
+    in
+    let s1 = String.sub s 0 half and s2 = String.sub s half half in
+    to_set s1, to_set s2
+
+  let line_score (set1, set2) =
+    let inter = Char_set.inter set1 set2 in
+    match Char_set.elements inter with
+    | [x] -> score x
+    | [] -> failf "No common element"
+    | a :: b :: _ -> failf "More than one common element: %c and %c" a b
+
+  let () =
+    let add_score score s = score + line_score (parse s) in
+    let score = Input.fold add_score 0 "3/data/input" in
+    Format.printf "@[score=%d@.@]" score
+end
+
+let pp_set ppf set =
+  let comma ppf () = Format.fprintf ppf ",@ " in
+  let char ppf c = Format.fprintf ppf "%c" c in
+  Format.fprintf ppf "{%a}"
+  (Format.pp_print_seq ~pp_sep:comma char) (Char_set.to_seq set)
+
+let badge array =
+  let (^) = Char_set.inter in
+  let inter = array.(0) ^ array.(1) ^ array.(2) in
+  match Char_set.elements inter with
+  | [x] -> x
+  | [] -> failf "@[No badge: %a, %a, %a@]"
+            pp_set array.(0)
+            pp_set array.(1)
+            pp_set array.(2)
+  | a :: b :: _ -> failf "Multiple badge found: %c %c?" a b
+
+let parse = Array.map to_set
+
+  let () =
+    let add_score sc s = sc + score (badge @@ parse s) in
+    let score = Input.group_by 3 add_score 0 "3/data/input" in
+    Format.printf "@[score=%d@.@]" score
--- 2022/lib/input.ml
+++ 2022/lib/input.ml
@@ -5,3 +5,16 @@ let fold f start filename =
       | None -> acc
   in
   In_channel.with_open_bin filename (fun ch -> loop f start ch)
+
+let group_by k f start filename =
+  let init state = (0, Array.make k "", state) in
+  let folder (counter, array, inner) x =
+    let () = array.(counter) <- x in
+    let counter = succ counter in
+    if counter = k then
+      init (f inner array)
+    else
+      (counter, array, inner)
+  in
+  let _,_, result = fold folder (init start) filename in
+  result
